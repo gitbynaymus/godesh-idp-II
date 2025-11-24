53f6da82ee1f23b9308f5add0fb841e946112f8f
Forhad Reza <forhad.bimt@gmail.com>
2025-11-24 20:51:24 +0600

    Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,101 @@
+import { Link } from "react-router";
+import Logo from "./shared/Logo";
+import { FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="divide-y bg-green-50 dark:bg-gray-100 dark:text-primary">
+      <div className="mx-auto flex w-11/12 max-w-[1440px] flex-col justify-between space-y-8 py-10 lg:flex-row lg:space-y-0">
+        <div className="lg:w-1/3">
+          <Logo />
+        </div>
+        <div className="grid grid-cols-2 gap-x-3 gap-y-8 text-sm sm:grid-cols-4 lg:w-2/3">
+          <div className="space-y-3">
+            <h3 className="tracking-wide uppercase dark:text-primary">
+              Product
+            </h3>
+            <ul className="space-y-1">
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Features
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Integrations
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  FAQ
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <h3 className="tracking-wide uppercase dark:text-primary">
+              Company
+            </h3>
+            <ul className="space-y-1">
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Terms of Service
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <h3 className="uppercase dark:text-primary">Developers</h3>
+            <ul className="space-y-1">
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Public API
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a rel="noopener noreferrer" href="#">
+                  Guides
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="space-y-3">
+            <div className="uppercase dark:text-primary">Social media</div>
+            <div className="flex justify-start space-x-4">
+              <Link>
+                <FaFacebook size={22} />
+              </Link>
+              <Link>
+                <FaInstagramSquare size={22} />
+              </Link>
+              <Link>
+                <FaTwitter size={22} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-6 text-center text-sm dark:text-gray-600">
+        © 2025 GoDesh All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
